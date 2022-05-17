6a5e151bc7682e1422c51c8a386f9e11877cdca3
Abhilash John <abhilashjohn07@gmail.com>
2022-05-17 13:14:20 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} All things invoices`}
+      description="Enscylopedia of all things invoices and taxation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
