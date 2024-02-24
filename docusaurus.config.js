5009d0319e4a35e9b080f6214e3f5d330a1ed57c
Abhilash John <abhilashjohn07@gmail.com>
2024-02-24 12:43:27 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,6 +60,12 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+         sitemap: {
+        changefreq: 'weekly',
+        priority: 0.5,
+        ignorePatterns: ['/tags/'],
+        filename: 'sitemap.xml',
+      },
       }),
     ],
   ],
