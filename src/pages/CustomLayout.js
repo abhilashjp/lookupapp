9eafa1cbd271fb8c92b759a9ca224f8f1f40e900
Abhilash John <abhilashjohn07@gmail.com>
2024-03-02 13:17:16 +0530

    Create CustomLayout.js

---
+++ src/pages/CustomLayout.js
@@ -0,0 +1,29 @@
+// CustomLayout.js
+
+import React from 'react';
+
+const CustomLayout = ({ children }) => {
+  return (
+    <html>
+      <head>
+        <title>Your Site Title</title>
+        {/* Other meta tags, stylesheets, etc. */}
+        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+        <script>
+          {`
+            hbspt.forms.create({
+              region: "na1",
+              portalId: "22529954",
+              formId: "a16f7dad-52b2-4450-8d85-fac6da7562e0"
+            });
+          `}
+        </script>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default CustomLayout;
