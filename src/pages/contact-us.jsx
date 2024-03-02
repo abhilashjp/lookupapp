ec7248394114b87eea543b3dd8236d3554db9ac6
Abhilash John <abhilashjohn07@gmail.com>
2024-03-02 13:32:29 +0530

    Update and rename contact-us.md to contact-us.jsx

---
+++ src/pages/contact-us.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+const MyPage = () => (
+  <>
+    <script
+      charset="utf-8"  
+      type="text/javascript"
+      src="//js.hsforms.net/forms/embed/v2.js"
+    ></script>
+    <script>
+      hbspt.forms.create({
+        region: "na1",
+        portalId: "22529954",
+        formId: "a16f7dad-52b2-4450-8d85-fac6da7562e0"  
+      });
+    </script>
+
+    {/* rest of page content */}
+  </>
+);
+
+export default MyPage;
