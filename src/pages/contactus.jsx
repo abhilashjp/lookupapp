8c6825ad5517788baa0cd4a315ebdb12063b6890
Abhilash John <abhilashjohn07@gmail.com>
2024-03-02 13:40:34 +0530

    Update contactus.jsx

--- src/pages/contactus.jsx
+++ src/pages/contactus.jsx
@@ -10,9 +10,9 @@ const contactus = () => (
     ></script>
     <script>
       hbspt.forms.create({
-        "region": "na1",
-        "portalId": "22529954",
-        "formId": "a16f7dad-52b2-4450-8d85-fac6da7562e0"  
+        'region': 'na1',
+        'portalId': '22529954',
+        'formId': 'a16f7dad-52b2-4450-8d85-fac6da7562e0'  
       });
     </script>
 
