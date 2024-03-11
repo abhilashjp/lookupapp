38663dc0f06283dde6578c465d0c8afd134966e0
Abhilash John <abhilashjohn07@gmail.com>
2024-03-11 15:56:06 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ customFields: {
       ],
     },
   ],
-}
+},
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
