f26bc9c98b4c7cad5a42809a147cf962d9f0c063
Abhilash John <abhilashjohn07@gmail.com>
2024-03-11 16:18:45 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,14 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "abhilashjp", // Usually your GitHub org/user name.
   projectName: "lookupapp", // Usually your repo name.
-customFields: {
-headers:  {
-  "route": "/tags/**",
-  "headers": {
-    "X-Robots-Tag": "noindex"
-  }
-},
-  
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
